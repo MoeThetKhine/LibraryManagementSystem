5f2579dad781d22c6944e8ab56f1dc1cfc777ebe
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 23:43:19 +0630

    Feat : Update User Model

--- LibraryManagementSystem.Domain/Models/User/UserModel.cs
+++ LibraryManagementSystem.Domain/Models/User/UserModel.cs
@@ -4,21 +4,14 @@
 
 public class UserModel
 {
-	public string UserId { get; set; } = null!;
-
 	public string UserName { get; set; } = null!;
 
 	public string Email { get; set; } = null!;
-
-	public string Password { get; set; } = null!;
-
-	public string UserRole { get; set; } = null!;
 
 	public string PhoneNumber { get; set; } = null!;
 
 	public string Address { get; set; } = null!;
 
-	public bool IsActive { get; set; }
 }
 
 #endregion
