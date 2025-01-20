3df6d9ed3f04785017657dd374c98cb94b483440
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:10:34 +0630

    Feat: Add BookModel

--- LibraryManagementSystem.Domain/Models/Book/BookModel.cs
+++ LibraryManagementSystem.Domain/Models/Book/BookModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Models.Book
 {
-	internal class BookModel
+	public class BookModel
 	{
+		public string BookId { get; set; } = null!;
+
+		public string Title { get; set; } = null!;
+
+		public string Author { get; set; } = null!;
+
+		public string Isbn { get; set; } = null!;
+
+		public string CategoryName { get; set; } = null!;
+
+		public int Qty { get; set; }
+
+		public decimal Price { get; set; }
+
+		public bool IsActive { get; set; }
 	}
 }
