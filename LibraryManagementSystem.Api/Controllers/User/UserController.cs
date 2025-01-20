cf7aee43164cba8179384ca7018b71669f815044
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:38:53 +0630

    Feat : Add LoginUserAsync in UserController

---
+++ LibraryManagementSystem.Api/Controllers/User/UserController.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManagementSystem.Domain.Features.User;
+using LibraryManagementSystem.Domain.Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Api.Controllers.User
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly UserService _userService;
+
+		public UserController(UserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> LoginUserAsync([FromForm]LoginUserModel loginUser)
+		{
+			var result = await _userService.LoginUserAsync(loginUser);
+			return Ok(result);
+		}
+
+	}
+}
