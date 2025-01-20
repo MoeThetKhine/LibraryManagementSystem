aced36c68fe3d1a9b2deb1ff7e70eceb4a16427b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 17:47:32 +0630

    Change Namespace

---
+++ LibraryManagementSystem.Database/AppDbContextModels/TblUser.cs
@@ -0,0 +1,24 @@
+﻿namespace LibraryManagementSystem.Database.AppDbContextModels;
+
+#region TblUser
+
+public partial class TblUser
+{
+	public string UserId { get; set; } = null!;
+
+	public string UserName { get; set; } = null!;
+
+	public string Email { get; set; } = null!;
+
+	public string Password { get; set; } = null!;
+
+	public string UserRole { get; set; } = null!;
+
+	public string PhoneNumber { get; set; } = null!;
+
+	public string Address { get; set; } = null!;
+
+	public bool IsActive { get; set; }
+}
+
+#endregion
