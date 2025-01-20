042591dbcd96419a8e4befe128e7b4ebe8ce287f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:47:09 +0630

    Feat : Add EnumRespType

---
+++ LibraryManagementSystem.Domain/Models/EnumRespType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Models
+{
+	public enum EnumRespType
+	{
+		None,
+		Success,
+		ValidationError,
+		SystemError
+	}
+
+}
