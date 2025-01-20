8b934b7005d7f2ce2c4377b4bf0d04ce77239655
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:46:44 +0630

    Format Code

--- LibraryManagementSystem.Domain/Features/User/UserService.cs
+++ LibraryManagementSystem.Domain/Features/User/UserService.cs
@@ -3,44 +3,43 @@ using LibraryManagementSystem.Domain.Models.User;
 using LibraryManagementSystem.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace LibraryManagementSystem.Domain.Features.User
+namespace LibraryManagementSystem.Domain.Features.User;
+
+public class UserService
 {
-	public class UserService
+	private readonly AppDbContext _appDbContext;
+
+	public UserService(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext _appDbContext;
-
-		public UserService(AppDbContext appDbContext)
-		{
-			_appDbContext = appDbContext;
-		}
-
-		#region Login User Async
-
-		public async Task<Result<LoginUserModel>> LoginUserAsync(LoginUserModel loginUser)
-		{
-			Result<LoginUserModel> result;
-
-			try
-			{
-				var user = await _appDbContext.TblUsers
-					.FirstOrDefaultAsync(u => u.Email == loginUser.Email && u.Password == loginUser.Password && !u.IsActive);
-
-				if(user is null)
-				{
-					result = Result<LoginUserModel>.ValidationError("Invalid email or password.");
-				}
-
-				result = Result<LoginUserModel>.Success(loginUser, "Login Successful.");
-			}
-			catch (Exception ex)
-			{
-				result = Result<LoginUserModel>.ValidationError(ex.Message);
-			}
-
-			return result;
-		}
-
-		#endregion
-
+		_appDbContext = appDbContext;
 	}
+
+	#region Login User Async
+
+	public async Task<Result<LoginUserModel>> LoginUserAsync(LoginUserModel loginUser)
+	{
+		Result<LoginUserModel> result;
+
+		try
+		{
+			var user = await _appDbContext.TblUsers
+				.FirstOrDefaultAsync(u => u.Email == loginUser.Email && u.Password == loginUser.Password && !u.IsActive);
+
+			if(user is null)
+			{
+				result = Result<LoginUserModel>.ValidationError("Invalid email or password.");
+			}
+
+			result = Result<LoginUserModel>.Success(loginUser, "Login Successful.");
+		}
+		catch (Exception ex)
+		{
+			result = Result<LoginUserModel>.ValidationError(ex.Message);
+		}
+
+		return result;
+	}
+
+	#endregion
+
 }
