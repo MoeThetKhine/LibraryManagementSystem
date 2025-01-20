07a9c5ed4ee4a51552a9c1c975c8392655c5809f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:38:16 +0630

    Feat : Add DI

--- LibraryManagementSystem.Api/Program.cs
+++ LibraryManagementSystem.Api/Program.cs
@@ -1,3 +1,7 @@
+using LibraryManagementSystem.Database.AppDbContextModels;
+using LibraryManagementSystem.Domain.Features.User;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+builder.Services.AddScoped<UserService>();
+
 
 var app = builder.Build();
 
