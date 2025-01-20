8d71a3357506627aff0b787e7950b6663b139ba0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:12:18 +0630

    Feat : Add Uesr Model

--- LibraryManagementSystem.Domain/Models/User/UserModel.cs
+++ LibraryManagementSystem.Domain/Models/User/UserModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Models.User
 {
-	internal class UserModel
+	public class UserModel
 	{
+		public string UserId { get; set; } = null!;
+
+		public string UserName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public string UserRole { get; set; } = null!;
+
+		public string PhoneNumber { get; set; } = null!;
+
+		public string Address { get; set; } = null!;
+
+		public bool IsActive { get; set; }
 	}
 }
