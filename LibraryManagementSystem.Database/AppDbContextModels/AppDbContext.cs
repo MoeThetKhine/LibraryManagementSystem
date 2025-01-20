baeb9ecb1f9a502fcc40a5b16d3768c72125cdc8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 17:46:53 +0630

    Change Namespace

---
+++ LibraryManagementSystem.Database/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Database.AppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+	public AppDbContext()
+	{
+	}
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+	}
+
+	public virtual DbSet<TblBook> TblBooks { get; set; }
+
+	public virtual DbSet<TblCategory> TblCategories { get; set; }
+
+	public virtual DbSet<TblTransaction> TblTransactions { get; set; }
+
+	public virtual DbSet<TblUser> TblUsers { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+		=> optionsBuilder.UseSqlServer("Server=.;Database=LibraryManagementSystem;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<TblBook>(entity =>
+		{
+			entity.HasKey(e => e.BookId).HasName("PK__Tbl_Book__3DE0C207F94EE4EA");
+
+			entity.ToTable("Tbl_Book");
+
+			entity.Property(e => e.BookId).HasMaxLength(50);
+			entity.Property(e => e.Author).HasMaxLength(255);
+			entity.Property(e => e.CategoryName).HasMaxLength(255);
+			entity.Property(e => e.IsActive).HasDefaultValue(true);
+			entity.Property(e => e.Isbn)
+				.HasMaxLength(20)
+				.HasColumnName("ISBN");
+			entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+			entity.Property(e => e.Title).HasMaxLength(255);
+		});
+
+		modelBuilder.Entity<TblCategory>(entity =>
+		{
+			entity.HasKey(e => e.CategoryId).HasName("PK__Tbl_Cate__19093A0B1C0155F3");
+
+			entity.ToTable("Tbl_Category");
+
+			entity.HasIndex(e => e.CategoryName, "UQ__Tbl_Cate__8517B2E04D64C716").IsUnique();
+
+			entity.Property(e => e.CategoryId).HasMaxLength(50);
+			entity.Property(e => e.CategoryName).HasMaxLength(255);
+		});
+
+		modelBuilder.Entity<TblTransaction>(entity =>
+		{
+			entity.HasKey(e => e.TransactionId).HasName("PK__Tbl_Tran__55433A6BBC26F307");
+
+			entity.ToTable("Tbl_Transaction");
+
+			entity.Property(e => e.TransactionId).HasMaxLength(50);
+			entity.Property(e => e.BookId).HasMaxLength(50);
+			entity.Property(e => e.BorrowDate).HasColumnType("datetime");
+			entity.Property(e => e.DueDate).HasColumnType("datetime");
+			entity.Property(e => e.Fine).HasColumnType("decimal(10, 2)");
+			entity.Property(e => e.Qty).HasDefaultValue(1);
+			entity.Property(e => e.ReturnDate).HasColumnType("datetime");
+			entity.Property(e => e.TotalAmount).HasColumnType("decimal(10, 2)");
+			entity.Property(e => e.UserName).HasMaxLength(255);
+		});
+
+		modelBuilder.Entity<TblUser>(entity =>
+		{
+			entity.HasKey(e => e.UserId).HasName("PK__Tbl_User__1788CC4CB1537EFB");
+
+			entity.ToTable("Tbl_User");
+
+			entity.HasIndex(e => e.Email, "UQ__Tbl_User__A9D105344BF41190").IsUnique();
+
+			entity.Property(e => e.UserId).HasMaxLength(50);
+			entity.Property(e => e.Address).HasMaxLength(255);
+			entity.Property(e => e.Email).HasMaxLength(255);
+			entity.Property(e => e.IsActive).HasDefaultValue(true);
+			entity.Property(e => e.Password).HasMaxLength(255);
+			entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+			entity.Property(e => e.UserName).HasMaxLength(255);
+			entity.Property(e => e.UserRole).HasMaxLength(50);
+		});
+
+		OnModelCreatingPartial(modelBuilder);
+	}
+
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
