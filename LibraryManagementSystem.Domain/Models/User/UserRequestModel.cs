230c4c17dd2a66d996e988e311ecfa773bb38c2b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:59:42 +0630

    Format Code

--- LibraryManagementSystem.Domain/Models/User/UserRequestModel.cs
+++ LibraryManagementSystem.Domain/Models/User/UserRequestModel.cs
@@ -1,16 +1,15 @@
-﻿namespace LibraryManagementSystem.Domain.Models.User
+﻿namespace LibraryManagementSystem.Domain.Models.User;
+
+public class UserRequestModel
 {
-	public class UserRequestModel
-	{
-		public string UserName { get; set; } = null!;
+	public string UserName { get; set; } = null!;
 
-		public string Email { get; set; } = null!;
+	public string Email { get; set; } = null!;
 
-		public string Password { get; set; } = null!;
+	public string Password { get; set; } = null!;
 
-		public string PhoneNumber { get; set; } = null!;
+	public string PhoneNumber { get; set; } = null!;
 
-		public string Address { get; set; } = null!;
+	public string Address { get; set; } = null!;
 
-	}
 }
