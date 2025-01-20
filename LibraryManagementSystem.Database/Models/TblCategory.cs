5693c006873591e8cdbc20ac8fb8818e4a5d3ca8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 15:38:21 +0630

    Feat : Add AppDbContext and Tables using Scaffold

---
+++ LibraryManagementSystem.Database/Models/TblCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Database.Models;
+
+public partial class TblCategory
+{
+    public string CategoryId { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+}
---
+++ LibraryManagementSystem.Database/Models/TblUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Database.Models;
+
+public partial class TblUser
+{
+    public string UserId { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string UserRole { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
