b35a895af8f438c4ccf606a07fddd20546790b3c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:07:13 +0630

    -

--- LibraryManagementSystem.Domain/Models/User/LoginUserModel.cs
+++ LibraryManagementSystem.Domain/Models/User/LoginUserModel.cs
@@ -1,9 +1,12 @@
 ﻿namespace LibraryManagementSystem.Domain.Models.User;
 
+#region Login User Model
+
 public class LoginUserModel
 {
 	public string Email { get; set; } = null!;
 
 	public string Password { get; set; } = null!;
-
 }
+
+#endregion
