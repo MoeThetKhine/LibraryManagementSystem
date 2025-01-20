176b4a347589dfbaa380acab42ba83de948dc655
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 22:15:24 +0630

    Feat : Add   User Login Async in User Service

--- LibraryManagementSystem.Domain/Features/User/UserService.cs
+++ LibraryManagementSystem.Domain/Features/User/UserService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LibraryManagementSystem.Database.AppDbContextModels;
+using LibraryManagementSystem.Domain.Models.User;
+using LibraryManagementSystem.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,38 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Features.User
 {
-	internal class UserService
+	public class UserService
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public UserService(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<Result<LoginUserModel>> LoginUserAsync(LoginUserModel loginUser)
+		{
+			Result<LoginUserModel> result;
+
+			try
+			{
+				var user = await _appDbContext.TblUsers
+					.FirstOrDefaultAsync(u => u.Email == loginUser.Email && u.Password == loginUser.Password && !u.IsActive);
+
+				if(user is null)
+				{
+					result = Result<LoginUserModel>.ValidationError("Invalid email or password.");
+				}
+
+				result = Result<LoginUserModel>.Success(loginUser, "Login Successful.");
+			}
+			catch (Exception ex)
+			{
+				result = Result<LoginUserModel>.ValidationError(ex.Message);
+			}
+
+			return result;
+		}
+
 	}
 }
