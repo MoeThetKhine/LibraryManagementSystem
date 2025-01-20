5a9250a7317459cd943c4580e31648c314230b46
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:14:58 +0630

    -

--- LibraryManagementSystem.Domain/Models/Book/BookModel.cs
+++ LibraryManagementSystem.Domain/Models/Book/BookModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibraryManagementSystem.Domain.Models.Book
+﻿namespace LibraryManagementSystem.Domain.Models.Book
 {
 	public class BookModel
 	{
