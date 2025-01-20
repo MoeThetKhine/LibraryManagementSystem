1c527226bfb3a516e4e3b35d81986159703d1de3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:52:47 +0630

    -

--- LibraryManagementSystem.Domain/Models/Result.cs
+++ LibraryManagementSystem.Domain/Models/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibraryManagementSystem.Domain.Models
+﻿namespace LibraryManagementSystem.Domain.Models
 {
 	public class Result<T>
 	{
