3b19cbfa5b0679b46ceb664937a83afee69ca3fd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:30:25 +0630

    Format Code

--- LibraryManagementSystem.Domain/Models/Book/BookModel.cs
+++ LibraryManagementSystem.Domain/Models/Book/BookModel.cs
@@ -1,21 +1,20 @@
-﻿namespace LibraryManagementSystem.Domain.Models.Book
+﻿namespace LibraryManagementSystem.Domain.Models.Book;
+
+public class BookModel
 {
-	public class BookModel
-	{
-		public string BookId { get; set; } = null!;
+	public string BookId { get; set; } = null!;
 
-		public string Title { get; set; } = null!;
+	public string Title { get; set; } = null!;
 
-		public string Author { get; set; } = null!;
+	public string Author { get; set; } = null!;
 
-		public string Isbn { get; set; } = null!;
+	public string Isbn { get; set; } = null!;
 
-		public string CategoryName { get; set; } = null!;
+	public string CategoryName { get; set; } = null!;
 
-		public int Qty { get; set; }
+	public int Qty { get; set; }
 
-		public decimal Price { get; set; }
+	public decimal Price { get; set; }
 
-		public bool IsActive { get; set; }
-	}
+	public bool IsActive { get; set; }
 }
