c845aab07a89f034ae7a109817db0c6221092663
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 21:50:27 +0630

    Feat : Add Result

--- LibraryManagementSystem.Domain/Models/Result.cs
+++ LibraryManagementSystem.Domain/Models/Result.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Models
 {
-	public class Result
+	public class Result<T>
 	{
+		public bool IsSuccess { get; set; }
+		public bool IsError { get { return !IsSuccess; } }
+		public bool IsValidationError { get { return Type == EnumRespType.ValidationError; } }
+		public bool IsSystemError { get { return Type == EnumRespType.SystemError; } }
+		private EnumRespType Type { get; set; }
+		public T Data { get; set; }
+		public string Message { get; set; }
 	}
 }
