e647703092a0f68693f11075c115ba10f0ebf3af
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-20 17:47:21 +0630

    Change Namespace

---
+++ LibraryManagementSystem.Database/AppDbContextModels/TblTransaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Database.AppDbContextModels;
+
+public partial class TblTransaction
+{
+	public string TransactionId { get; set; } = null!;
+
+	public string UserName { get; set; } = null!;
+
+	public string BookId { get; set; } = null!;
+
+	public DateTime BorrowDate { get; set; }
+
+	public DateTime DueDate { get; set; }
+
+	public DateTime ReturnDate { get; set; }
+
+	public decimal Fine { get; set; }
+
+	public int Qty { get; set; }
+
+	public decimal TotalAmount { get; set; }
+}
