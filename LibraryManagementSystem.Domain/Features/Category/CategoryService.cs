281a7dedb8d8a95cc966d3595292b260f03beacb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 13:52:04 +0630

    Feat : Add CreateCategoryAsync in CategoryService

--- LibraryManagementSystem.Domain/Features/Category/CategoryService.cs
+++ LibraryManagementSystem.Domain/Features/Category/CategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Domain.Models.Category;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Features.Category
 {
-	internal class CategoryService
+	public class CategoryService
 	{
+		private readonly AppDbContext appDbContext;
+
+		public CategoryService(AppDbContext appDbContext)
+		{
+			this.appDbContext = appDbContext;
+		}
+
+		public async Task<Result<CategoryRequestModel>> CreateCategory(CategoryRequestModel requestModel)
+		{
+			Result<CategoryRequestModel> result;
+			try
+			{
+				var category = new TblCategory
+				{
+					CategoryId = Guid.NewGuid().ToString(),
+					CategoryName = requestModel.CategoryName,
+					Description = requestModel.Description
+				};
+
+				await appDbContext.TblCategories.AddAsync(category);
+				await appDbContext.SaveChangesAsync();
+
+				result = Result<CategoryRequestModel>.Success(requestModel, "Creating Successful.");
+			}
+
+			catch(Exception ex)
+			{
+				result = Result<CategoryRequestModel>.ValidationError(ex.Message);
+			}	
+
+			return result;
+			
+		}
 	}
 }
