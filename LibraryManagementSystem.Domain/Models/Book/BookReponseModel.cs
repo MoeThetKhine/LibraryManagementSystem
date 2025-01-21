f389236e4ceb1cf34b560bc1346843b1786a0424
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 19:53:36 +0630

    -

--- LibraryManagementSystem.Domain/Models/Book/BookReponseModel.cs
+++ LibraryManagementSystem.Domain/Models/Book/BookReponseModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace LibraryManagementSystem.Domain.Models.Book;
 
+#region Book Reponse Model
+
 public class BookReponseModel
 {
 	public int Qty { get; set; }
 
 	public decimal Price { get; set; }
 }
+
+#endregion
