92ad27d049472084115ff98676f1f5dd835cd70e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 16:06:45 +0630

    Feat : Add BookService in Program

--- LibraryManagementSystem.Api/Program.cs
+++ LibraryManagementSystem.Api/Program.cs
@@ -1,3 +1,5 @@
+using LibraryManagementSystem.Domain.Features.Book;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -12,6 +14,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<CategoryService>();	
+builder.Services.AddScoped<BookService>();
 
 #endregion
 
