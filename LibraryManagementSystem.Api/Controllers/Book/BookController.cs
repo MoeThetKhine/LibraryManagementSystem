360a473e4e347f58f631dca937df56de969b2940
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 16:45:55 +0630

    Format Code

--- LibraryManagementSystem.Api/Controllers/Book/BookController.cs
+++ LibraryManagementSystem.Api/Controllers/Book/BookController.cs
@@ -1,21 +1,20 @@
-﻿namespace LibraryManagementSystem.Api.Controllers.Book
+﻿namespace LibraryManagementSystem.Api.Controllers.Book;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BookController : ControllerBase
+	private readonly BookService _bookService;
+
+	public BookController(BookService bookService)
 	{
-		private readonly BookService _bookService;
+		_bookService = bookService;
+	}
 
-		public BookController(BookService bookService)
-		{
-			_bookService = bookService;
-		}
-
-		[HttpGet]
-		public async Task<IActionResult> GetBookAsync()
-		{
-			var result = await _bookService.GetBookAsync();
-			return Ok(result);
-		}
+	[HttpGet]
+	public async Task<IActionResult> GetBookAsync()
+	{
+		var result = await _bookService.GetBookAsync();
+		return Ok(result);
 	}
 }
