cca32400acecd1de6314281b8f0a096a02f5c8c7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 16:07:20 +0630

    Feat : Changing Name

---
+++ LibraryManagementSystem.Domain/Features/Book/BookService.cs
@@ -0,0 +1,35 @@
+﻿using LibraryManagementSystem.Domain.Models.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Features.Book
+{
+	public class BookService
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public BookService(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<Result<IEnumerable<BookModel>>> GetBookAsync()
+		{
+			Result<IEnumerable<BookModel>> result;
+
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				result = Result<IEnumerable<BookModel>>.ValidationError(ex.Message);
+			}
+
+			return result;
+		}
+	}
+}
