832ef2865da543e8c60c247ec88ba8d83315134f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 13:57:35 +0630

    -

--- LibraryManagementSystem.Domain/Features/Category/CategoryService.cs
+++ LibraryManagementSystem.Domain/Features/Category/CategoryService.cs
@@ -1,43 +1,42 @@
-﻿namespace LibraryManagementSystem.Domain.Features.Category
+﻿namespace LibraryManagementSystem.Domain.Features.Category;
+
+public class CategoryService
 {
-	public class CategoryService
+	private readonly AppDbContext appDbContext;
+
+	public CategoryService(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext appDbContext;
-
-		public CategoryService(AppDbContext appDbContext)
-		{
-			this.appDbContext = appDbContext;
-		}
-
-		#region Create Category Async
-
-		public async Task<Result<CategoryRequestModel>> CreateCategoryAsync(CategoryRequestModel requestModel)
-		{
-			Result<CategoryRequestModel> result;
-			try
-			{
-				var category = new TblCategory
-				{
-					CategoryId = Guid.NewGuid().ToString(),
-					CategoryName = requestModel.CategoryName,
-					Description = requestModel.Description
-				};
-
-				await appDbContext.TblCategories.AddAsync(category);
-				await appDbContext.SaveChangesAsync();
-
-				result = Result<CategoryRequestModel>.Success(requestModel, "Creating Successful.");
-			}
-
-			catch(Exception ex)
-			{
-				result = Result<CategoryRequestModel>.ValidationError(ex.Message);
-			}	
-
-			return result;
-			
-		}
-
-		#endregion
+		this.appDbContext = appDbContext;
 	}
+
+	#region Create Category Async
+
+	public async Task<Result<CategoryRequestModel>> CreateCategoryAsync(CategoryRequestModel requestModel)
+	{
+		Result<CategoryRequestModel> result;
+		try
+		{
+			var category = new TblCategory
+			{
+				CategoryId = Guid.NewGuid().ToString(),
+				CategoryName = requestModel.CategoryName,
+				Description = requestModel.Description
+			};
+
+			await appDbContext.TblCategories.AddAsync(category);
+			await appDbContext.SaveChangesAsync();
+
+			result = Result<CategoryRequestModel>.Success(requestModel, "Creating Successful.");
+		}
+
+		catch(Exception ex)
+		{
+			result = Result<CategoryRequestModel>.ValidationError(ex.Message);
+		}	
+
+		return result;
+		
+	}
+
+	#endregion
 }
