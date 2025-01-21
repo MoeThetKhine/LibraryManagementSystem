c80721373afe59bd611a1825bddb4ac56e09e1c0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 16:36:25 +0630

    Format Code

--- LibraryManagementSystem.Domain/Features/Book/BookService.cs
+++ LibraryManagementSystem.Domain/Features/Book/BookService.cs
@@ -1,49 +1,48 @@
 ﻿using LibraryManagementSystem.Domain.Models.Book;
 
-namespace LibraryManagementSystem.Domain.Features.Book
+namespace LibraryManagementSystem.Domain.Features.Book;
+
+public class BookService
 {
-	public class BookService
+	private readonly AppDbContext _appDbContext;
+
+	public BookService(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext _appDbContext;
+		_appDbContext = appDbContext;
+	}
 
-		public BookService(AppDbContext appDbContext)
+	public async Task<Result<IEnumerable<BookModel>>> GetBookAsync()
+	{
+		Result<IEnumerable<BookModel>> result;
+
+		try
 		{
-			_appDbContext = appDbContext;
-		}
+			var books = await _appDbContext.TblBooks
+				.AsNoTracking()
+				.ToListAsync();
 
-		public async Task<Result<IEnumerable<BookModel>>> GetBookAsync()
-		{
-			Result<IEnumerable<BookModel>> result;
-
-			try
+			if (!books.Any())
 			{
-				var books = await _appDbContext.TblBooks
-					.AsNoTracking()
-					.ToListAsync();
-
-				if (!books.Any())
-				{
-					result = Result<IEnumerable<BookModel>>.ValidationError("No Book Found.");
-				}
-
-				var lst =  books.Select(book => new BookModel
-				{
-					Title = book.Title,
-					Author = book.Author,
-					Isbn = book.Isbn,
-					CategoryName = book.CategoryName,
-					Qty = book.Qty,
-					Price = book.Price
-				}).ToList();
-
-				result = Result<IEnumerable<BookModel>>.Success(lst);
-			}
-			catch (Exception ex)
-			{
-				result = Result<IEnumerable<BookModel>>.ValidationError(ex.Message);
+				result = Result<IEnumerable<BookModel>>.ValidationError("No Book Found.");
 			}
 
-			return result;
+			var lst =  books.Select(book => new BookModel
+			{
+				Title = book.Title,
+				Author = book.Author,
+				Isbn = book.Isbn,
+				CategoryName = book.CategoryName,
+				Qty = book.Qty,
+				Price = book.Price
+			}).ToList();
+
+			result = Result<IEnumerable<BookModel>>.Success(lst);
 		}
+		catch (Exception ex)
+		{
+			result = Result<IEnumerable<BookModel>>.ValidationError(ex.Message);
+		}
+
+		return result;
 	}
 }
