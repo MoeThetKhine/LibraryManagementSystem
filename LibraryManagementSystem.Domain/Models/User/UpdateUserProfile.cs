3df66588d565192f8e2fc9b3f1f413c4f04967a4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 11:39:41 +0630

    Format Code

--- LibraryManagementSystem.Domain/Models/User/UpdateUserProfile.cs
+++ LibraryManagementSystem.Domain/Models/User/UpdateUserProfile.cs
@@ -1,18 +1,17 @@
-﻿namespace LibraryManagementSystem.Domain.Models.User
+﻿namespace LibraryManagementSystem.Domain.Models.User;
+
+public class UpdateUserProfile
 {
-	public class UpdateUserProfile
-	{
-		public string UserName { get; set; } = null!;
+	public string UserName { get; set; } = null!;
 
-		public string Email { get; set; } = null!;
+	public string Email { get; set; } = null!;
 
-		public string Password { get; set; } = null!;
+	public string Password { get; set; } = null!;
 
-		public string UserRole { get; set; } = null!;
+	public string UserRole { get; set; } = null!;
 
-		public string PhoneNumber { get; set; } = null!;
+	public string PhoneNumber { get; set; } = null!;
 
-		public string Address { get; set; } = null!;
+	public string Address { get; set; } = null!;
 
-	}
 }
