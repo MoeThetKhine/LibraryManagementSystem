b9da200b61168461bae1711ac679e8ba5d1a05ae
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 13:59:26 +0630

    Format Code

--- LibraryManagementSystem.Api/Controllers/Category/CategoryController.cs
+++ LibraryManagementSystem.Api/Controllers/Category/CategoryController.cs
@@ -1,26 +1,25 @@
-﻿namespace LibraryManagementSystem.Api.Controllers.Category
+﻿namespace LibraryManagementSystem.Api.Controllers.Category;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class CategoryController : ControllerBase
+	private readonly CategoryService _categoryService;
+
+	public CategoryController(CategoryService categoryService)
 	{
-		private readonly CategoryService _categoryService;
-
-		public CategoryController(CategoryService categoryService)
-		{
-			_categoryService = categoryService;
-		}
-
-		#region Create Category Async
-
-		[HttpPost]
-		public async Task<IActionResult> CreateCategoryAsync(CategoryRequestModel requestModel)
-		{
-			var result = await _categoryService.CreateCategoryAsync(requestModel);
-			return Ok(result);
-		}
-
-		#endregion
-
+		_categoryService = categoryService;
 	}
+
+	#region Create Category Async
+
+	[HttpPost]
+	public async Task<IActionResult> CreateCategoryAsync(CategoryRequestModel requestModel)
+	{
+		var result = await _categoryService.CreateCategoryAsync(requestModel);
+		return Ok(result);
+	}
+
+	#endregion
+
 }
