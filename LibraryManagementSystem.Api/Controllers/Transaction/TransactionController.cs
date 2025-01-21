bafc80adb2344cf6b65efb2dc2c1769091d160f7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 23:58:34 +0630

    Feat : Add GetTransactionAsync in Controller

---
+++ LibraryManagementSystem.Api/Controllers/Transaction/TransactionController.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagementSystem.Domain.Features.Transaction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Api.Controllers.Transaction
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TransactionController : ControllerBase
+	{
+		private readonly TransactionService _transactionService;
+
+		public TransactionController(TransactionService transactionService)
+		{
+			_transactionService = transactionService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetTransactionAsync()
+		{
+			var result = await _transactionService.GetTransactionAsync();
+			return Ok(result);
+		}
+	}
+}
