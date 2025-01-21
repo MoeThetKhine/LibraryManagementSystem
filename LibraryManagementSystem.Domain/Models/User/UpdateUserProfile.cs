5d3a5af249f93db5722442755ddf2c41ab77744e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 13:15:14 +0630

    Feat : UpdateUserProfile

--- LibraryManagementSystem.Domain/Models/User/UpdateUserProfile.cs
+++ LibraryManagementSystem.Domain/Models/User/UpdateUserProfile.cs
@@ -4,17 +4,13 @@
 
 public class UpdateUserProfile
 {
-	public string UserName { get; set; } = null!;
+	public string UserName { get; set; } 
 
-	public string Email { get; set; } = null!;
+	public string Password { get; set; }
 
-	public string Password { get; set; } = null!;
+	public string PhoneNumber { get; set; } 
 
-	public string UserRole { get; set; } = null!;
-
-	public string PhoneNumber { get; set; } = null!;
-
-	public string Address { get; set; } = null!;
+	public string Address { get; set; }
 
 }
 
