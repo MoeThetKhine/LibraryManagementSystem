38b012f3c023652203b79d373ceeaed0e7015d8b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 13:52:31 +0630

    Feat : Add CreateCategoryAsync in Category Controller

---
+++ LibraryManagementSystem.Api/Controllers/Category/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagementSystem.Domain.Features.Category;
+using LibraryManagementSystem.Domain.Models.Category;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Api.Controllers.Category
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		private readonly CategoryService _categoryService;
+
+		public CategoryController(CategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateCategoryAsync(CategoryRequestModel requestModel)
+		{
+			var result = await _categoryService.CreateCategoryAsync(requestModel);
+			return Ok(result);
+		}
+	}
+}
