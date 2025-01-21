28ba8d0c4fa90d6d232475b0178032886cde88ec
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-21 16:05:17 +0630

    Feat : Update Category Model

---
+++ LibraryManagementSystem.Domain/Models/Category/CategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Models.Category
+{
+	public class CategoryModel
+	{
+		public string CategoryId { get; set; } = null!;
+
+		public string CategoryName { get; set; } = null!;
+
+		public string? Description { get; set; }
+	}
+}
