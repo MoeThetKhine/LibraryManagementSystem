a78a3bbd9fd62a28a5a9ac092161cceb2c06f03c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:09:45 +0630

    -

--- LibraryManagementSystem.Database/AppDbContextModels/AppDbContext.cs
+++ LibraryManagementSystem.Database/AppDbContextModels/AppDbContext.cs
@@ -11,7 +11,9 @@ public partial class AppDbContext : DbContext
     {
     }
 
-    public virtual DbSet<TblBook> TblBooks { get; set; }
+	#region DbSet
+
+	public virtual DbSet<TblBook> TblBooks { get; set; }
 
     public virtual DbSet<TblBorrow> TblBorrows { get; set; }
 
@@ -23,7 +25,11 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<TblUser> TblUsers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	#endregion
+
+
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.;Database=LibraryManagementSystem;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 
