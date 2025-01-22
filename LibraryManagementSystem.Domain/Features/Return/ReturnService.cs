9a6d83f306f617215f829ec4fc06ca8ba6714b0b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:56:51 +0630

    Feat : Add GetReturnListAsync in Service

--- LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
+++ LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Domain.Models.Return;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Features.Return
 {
-	internal class ReturnService
+	public class ReturnService
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public ReturnService(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<Result<IEnumerable<ReturnModel>>> GetReturnListAsync()
+		{
+			Result<IEnumerable<ReturnModel>> result;
+
+			try
+			{
+				var returns = await _appDbContext.TblReturns
+					.AsNoTracking()
+					.ToListAsync();
+
+				var lst = returns.Select(x => new ReturnModel
+				{
+					BorrowId = x.BorrowId,
+					ReturnDate = x.ReturnDate,
+					DaysLate = x.DaysLate,
+					Fine = x.Fine,
+					TotalAmount = x.TotalAmount
+				}).ToList();
+
+				if (!lst.Any())
+				{
+					result = Result<IEnumerable<ReturnModel>>.ValidationError("No Return Found.");
+				}
+				result = Result<IEnumerable<ReturnModel>>.Success(lst);
+
+			}
+			catch (Exception ex)
+			{
+				result = Result<IEnumerable<ReturnModel>>.ValidationError(ex.Message);
+			}
+			return result;
+		}
 	}
 }
