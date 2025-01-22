eb8282b228b7cbaa8a5e2a523cbce174e2393bd3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:57:47 +0630

    Format Code

--- LibraryManagementSystem.Api/Controllers/Return/ReturnController.cs
+++ LibraryManagementSystem.Api/Controllers/Return/ReturnController.cs
@@ -1,28 +1,27 @@
 ﻿using LibraryManagementSystem.Domain.Features.Return;
 
-namespace LibraryManagementSystem.Api.Controllers.Return
+namespace LibraryManagementSystem.Api.Controllers.Return;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReturnController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ReturnController : ControllerBase
+	private readonly ReturnService _returnService;
+
+	public ReturnController(ReturnService returnService)
 	{
-		private readonly ReturnService _returnService;
-
-		public ReturnController(ReturnService returnService)
-		{
-			_returnService = returnService;
-		}
-
-		#region Get Return List Async
-
-		[HttpGet]
-		public async Task<IActionResult> GetReturnListAsync()
-		{
-			var result = await _returnService.GetReturnListAsync();
-			return Ok(result);
-		}
-
-		#endregion
-
+		_returnService = returnService;
 	}
+
+	#region Get Return List Async
+
+	[HttpGet]
+	public async Task<IActionResult> GetReturnListAsync()
+	{
+		var result = await _returnService.GetReturnListAsync();
+		return Ok(result);
+	}
+
+	#endregion
+
 }
