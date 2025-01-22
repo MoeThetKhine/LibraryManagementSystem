e589dce94b848cfce757496bad09572a4f5997ea
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:57:00 +0630

    Feat : Add GetReturnListAsync in Controller

--- LibraryManagementSystem.Api/Controllers/Return/ReturnController.cs
+++ LibraryManagementSystem.Api/Controllers/Return/ReturnController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LibraryManagementSystem.Domain.Features.Return;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.Api.Controllers.Return
@@ -7,5 +8,18 @@ namespace LibraryManagementSystem.Api.Controllers.Return
 	[ApiController]
 	public class ReturnController : ControllerBase
 	{
+		private readonly ReturnService _returnService;
+
+		public ReturnController(ReturnService returnService)
+		{
+			_returnService = returnService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetReturnListAsync()
+		{
+			var result = await _returnService.GetReturnListAsync();
+			return Ok(result);
+		}			
 	}
 }
