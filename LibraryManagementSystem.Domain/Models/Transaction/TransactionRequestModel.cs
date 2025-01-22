d8cd7c64c0ab8bafbb171ea034cf062001247b0c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 08:43:45 +0630

    Feat : Add Transaction Request Model

---
+++ LibraryManagementSystem.Domain/Models/Transaction/TransactionRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Models.Transaction
+{
+	public class TransactionRequestModel
+	{
+		//public string TransactionId { get; set; } = null!;
+
+		public string UserName { get; set; } = null!;
+
+		public string BookId { get; set; } = null!;
+
+		public DateTime BorrowDate { get; set; }
+
+		public DateTime DueDate { get; set; }
+
+		public DateTime ReturnDate { get; set; }
+
+		public decimal Fine { get; set; }
+
+		public int Qty { get; set; }
+
+		public decimal TotalAmount { get; set; }
+	}
+}
