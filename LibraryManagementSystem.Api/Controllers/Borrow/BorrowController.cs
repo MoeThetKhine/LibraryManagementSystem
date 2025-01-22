4d32d62b66189a6bac11218cb73720e4df527972
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 15:18:43 +0630

    Format Code

--- LibraryManagementSystem.Api/Controllers/Borrow/BorrowController.cs
+++ LibraryManagementSystem.Api/Controllers/Borrow/BorrowController.cs
@@ -1,37 +1,36 @@
-﻿namespace LibraryManagementSystem.Api.Controllers.Borrow
+﻿namespace LibraryManagementSystem.Api.Controllers.Borrow;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BorrowController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BorrowController : ControllerBase
+	private readonly BorrowService _borrowService;
+
+	public BorrowController(BorrowService borrowService)
 	{
-		private readonly BorrowService _borrowService;
-
-		public BorrowController(BorrowService borrowService)
-		{
-			_borrowService = borrowService;
-		}
-
-		#region Get Borrow List Async
-
-		[HttpGet]
-		public async Task<IActionResult> GetBorrowListAsync()
-		{
-			var result = await _borrowService.GetBorrowListAsync();
-			return Ok(result);
-		}
-
-		#endregion
-
-		#region Get Borrow List By Id Async
-
-		[HttpGet("{id}")]
-		public async Task<IActionResult> GetBorrowListByIdAsync(string id)
-		{
-			var result = await _borrowService.GetBorrowListByIdAsync(id);
-			return Ok(result);
-		}
-
-		#endregion
-
+		_borrowService = borrowService;
 	}
+
+	#region Get Borrow List Async
+
+	[HttpGet]
+	public async Task<IActionResult> GetBorrowListAsync()
+	{
+		var result = await _borrowService.GetBorrowListAsync();
+		return Ok(result);
+	}
+
+	#endregion
+
+	#region Get Borrow List By Id Async
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetBorrowListByIdAsync(string id)
+	{
+		var result = await _borrowService.GetBorrowListByIdAsync(id);
+		return Ok(result);
+	}
+
+	#endregion
+
 }
