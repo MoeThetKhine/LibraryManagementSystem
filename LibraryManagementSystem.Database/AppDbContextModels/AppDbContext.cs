c34ea2ef8796b9e6312f948b15b52d56162d6131
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:12:44 +0630

    -

--- LibraryManagementSystem.Database/AppDbContextModels/AppDbContext.cs
+++ LibraryManagementSystem.Database/AppDbContextModels/AppDbContext.cs
@@ -33,9 +33,11 @@ public partial class AppDbContext : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.;Database=LibraryManagementSystem;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 
-    #endregion
+	#endregion
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	#region On Model Creating
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
 		#region TblBook
@@ -156,5 +158,7 @@ public partial class AppDbContext : DbContext
 		OnModelCreatingPartial(modelBuilder);
     }
 
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+	#endregion
+
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
