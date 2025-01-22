add98e97e5af390c3caf5f9723f22e47b8f530c6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 15:11:29 +0630

    Feat : Add GetBorrowListByIdAsync in Service

--- LibraryManagementSystem.Domain/Features/Borrow/BorrowService.cs
+++ LibraryManagementSystem.Domain/Features/Borrow/BorrowService.cs
@@ -47,5 +47,36 @@ namespace LibraryManagementSystem.Domain.Features.Borrow
 			return result;
 		}
 
+		public async Task<Result<IEnumerable<BorrowModel>>> GetBorrowListByIdAsync(string id)
+		{
+			Result<IEnumerable<BorrowModel>> result;
+
+			try
+			{
+				var borrow = await _appDbContext.TblBorrows.AsNoTracking()
+					.Where(x => x.BorrowId == id).ToListAsync();
+
+				var lst = borrow.Select(x => new BorrowModel
+				{
+					UserId = x.UserId,
+					BookId = x.BookId,
+					BorrowDate = x.BorrowDate,
+					DueDate = x.DueDate,
+					Qty = x.Qty,
+				}).ToList();
+
+				if (!lst.Any())
+				{
+					result = Result<IEnumerable<BorrowModel>>.ValidationError("No Return Found.");
+				}
+				result = Result<IEnumerable<BorrowModel>>.Success(lst);
+			}
+			catch (Exception ex)
+			{
+				result = Result<IEnumerable<BorrowModel>>.ValidationError(ex.Message);
+			}
+			return result;
+		}
+
 	}
 }
