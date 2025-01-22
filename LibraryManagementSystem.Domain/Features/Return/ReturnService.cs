5fb4714dbadab0395c164724a620ab2ceea28cbe
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 14:29:54 +0630

    Feat : Add GetReturnListByIdAsync in Service

--- LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
+++ LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
@@ -46,4 +46,37 @@ public class ReturnService
 
 	#endregion
 
+	public async Task<Result<IEnumerable<ReturnModel>>> GetReturnListByIdAsync(string id)
+	{
+		Result<IEnumerable<ReturnModel>> result;
+
+		try
+		{
+			var returns = await _appDbContext.TblReturns.AsNoTracking()
+				.Where(x=> x.ReturnId == id).ToListAsync();
+
+			var lst = returns.Select (x => new ReturnModel
+			{
+				BorrowId = x.BorrowId,
+				ReturnDate = x.ReturnDate,
+				DaysLate = x.DaysLate,
+				Fine = x.Fine,
+				TotalAmount = x.TotalAmount
+			}).ToList();
+
+
+			if (!lst.Any())
+			{
+				result = Result<IEnumerable<ReturnModel>>.ValidationError("No Return Found.");
+			}
+			result = Result<IEnumerable<ReturnModel>>.Success(lst);
+
+		}
+		catch (Exception ex)
+		{
+			result = Result<IEnumerable<ReturnModel>>.ValidationError(ex.Message);
+		}
+		return result;
+	}
+
 }
