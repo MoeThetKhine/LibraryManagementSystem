dba94b3d43d9fd0ed892e9c9be5100958f4887d5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:33:01 +0630

    Feat : Add Return Model

--- LibraryManagementSystem.Domain/Models/Return/ReturnModel.cs
+++ LibraryManagementSystem.Domain/Models/Return/ReturnModel.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Models.Return
 {
-	internal class ReturnModel
+	public class ReturnModel
 	{
+		public string ReturnId { get; set; } = null!;
+
+		public string BorrowId { get; set; } = null!;
+
+		public DateTime ReturnDate { get; set; }
+
+		public int DaysLate { get; set; }
+
+		public decimal Fine { get; set; }
+
+		public decimal TotalAmount { get; set; }
 	}
 }
