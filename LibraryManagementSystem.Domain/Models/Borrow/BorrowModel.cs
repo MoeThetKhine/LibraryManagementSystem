798a62dcfb945322edd315a63d473809bd00355e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:34:08 +0630

    Format Code

--- LibraryManagementSystem.Domain/Models/Borrow/BorrowModel.cs
+++ LibraryManagementSystem.Domain/Models/Borrow/BorrowModel.cs
@@ -1,17 +1,16 @@
-﻿namespace LibraryManagementSystem.Domain.Models.Borrow
+﻿namespace LibraryManagementSystem.Domain.Models.Borrow;
+
+public class BorrowModel
 {
-	public class BorrowModel
-	{
-		public string BorrowId { get; set; } = null!;
+	public string BorrowId { get; set; } = null!;
 
-		public string UserId { get; set; } = null!;
+	public string UserId { get; set; } = null!;
 
-		public string BookId { get; set; } = null!;
+	public string BookId { get; set; } = null!;
 
-		public DateTime BorrowDate { get; set; }
+	public DateTime BorrowDate { get; set; }
 
-		public DateTime DueDate { get; set; }
+	public DateTime DueDate { get; set; }
 
-		public int Qty { get; set; }
-	}
+	public int Qty { get; set; }
 }
