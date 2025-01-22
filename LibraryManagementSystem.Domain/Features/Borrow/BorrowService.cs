e614b8f651c4321f00469262c2c5f66ce046efe1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 14:44:55 +0630

    Feat : Add GetBorrowListAsync in Service

--- LibraryManagementSystem.Domain/Features/Borrow/BorrowService.cs
+++ LibraryManagementSystem.Domain/Features/Borrow/BorrowService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.Domain.Models.Borrow;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Features.Borrow
 {
-	internal class BorrowService
+	public class BorrowService
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public BorrowService(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<Result<IEnumerable<BorrowModel>>> GetBorrowListAsync()
+		{
+			Result<IEnumerable<BorrowModel>> result;
+
+			try
+			{
+				var borrow = await _appDbContext.TblBorrows.AsNoTracking()
+					.ToListAsync();
+
+				var lst = borrow.Select(x => new BorrowModel { 
+				UserId = x.UserId,
+				BookId = x.BookId,
+				BorrowDate = x.BorrowDate,
+				DueDate = x.DueDate,
+				Qty = x.Qty,
+				}).ToList();
+
+
+				if (!lst.Any())
+				{
+					result = Result<IEnumerable<BorrowModel>>.ValidationError("No Return Found.");
+				}
+				result = Result<IEnumerable<BorrowModel>>.Success(lst);
+			}
+			catch (Exception ex)
+			{
+				result = Result<IEnumerable<BorrowModel>>.ValidationError(ex.Message);
+			}
+			return result;
+		}
+
 	}
 }
