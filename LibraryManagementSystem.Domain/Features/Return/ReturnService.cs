e1df708e36125bade9dcdfd374698605eeb288e8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-22 11:58:22 +0630

    Format Code

--- LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
+++ LibraryManagementSystem.Domain/Features/Return/ReturnService.cs
@@ -5,48 +5,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagementSystem.Domain.Features.Return
+namespace LibraryManagementSystem.Domain.Features.Return;
+
+public class ReturnService
 {
-	public class ReturnService
+	private readonly AppDbContext _appDbContext;
+
+	public ReturnService(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext _appDbContext;
+		_appDbContext = appDbContext;
+	}
 
-		public ReturnService(AppDbContext appDbContext)
+	public async Task<Result<IEnumerable<ReturnModel>>> GetReturnListAsync()
+	{
+		Result<IEnumerable<ReturnModel>> result;
+
+		try
 		{
-			_appDbContext = appDbContext;
-		}
+			var returns = await _appDbContext.TblReturns
+				.AsNoTracking()
+				.ToListAsync();
 
-		public async Task<Result<IEnumerable<ReturnModel>>> GetReturnListAsync()
+			var lst = returns.Select(x => new ReturnModel
+			{
+				BorrowId = x.BorrowId,
+				ReturnDate = x.ReturnDate,
+				DaysLate = x.DaysLate,
+				Fine = x.Fine,
+				TotalAmount = x.TotalAmount
+			}).ToList();
+
+			if (!lst.Any())
+			{
+				result = Result<IEnumerable<ReturnModel>>.ValidationError("No Return Found.");
+			}
+			result = Result<IEnumerable<ReturnModel>>.Success(lst);
+
+		}
+		catch (Exception ex)
 		{
-			Result<IEnumerable<ReturnModel>> result;
-
-			try
-			{
-				var returns = await _appDbContext.TblReturns
-					.AsNoTracking()
-					.ToListAsync();
-
-				var lst = returns.Select(x => new ReturnModel
-				{
-					BorrowId = x.BorrowId,
-					ReturnDate = x.ReturnDate,
-					DaysLate = x.DaysLate,
-					Fine = x.Fine,
-					TotalAmount = x.TotalAmount
-				}).ToList();
-
-				if (!lst.Any())
-				{
-					result = Result<IEnumerable<ReturnModel>>.ValidationError("No Return Found.");
-				}
-				result = Result<IEnumerable<ReturnModel>>.Success(lst);
-
-			}
-			catch (Exception ex)
-			{
-				result = Result<IEnumerable<ReturnModel>>.ValidationError(ex.Message);
-			}
-			return result;
+			result = Result<IEnumerable<ReturnModel>>.ValidationError(ex.Message);
 		}
+		return result;
 	}
 }
